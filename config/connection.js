f1e5da84b43432f906c3dde48457eb3077327542
Jorden Ragels <jordenragels3@gmail.com>
2020-01-12 16:32:21 -0700

    connection addition

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,17 @@ connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: 'root',
+        password: 'hacktheplanet',
+        database: 'todoagain_db'
+    });
+};
+
 connection.connect(function(err) {
     if (err) {
         console.log("error connecting: " + err.stack);
